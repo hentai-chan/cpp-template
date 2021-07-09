201ff8209012b891abb0947318391fbb2d8922d9
hentai-chan <70948244+hentai-chan@users.noreply.github.com>
2021-07-09 21:05:27 +0200

    Add shell helper script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [[ "$1" == "config" ]]; then
+	cmake . -B out/debug -G Ninja -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+elif [[ "$1" == "build" ]]; then
+	cmake --build out/debug
+elif [[ "$1" == "tidy" ]]; then
+	python run-clang-tidy.py -config='' -p ./out/debug -header-filter='.*' ./src/*.[cp]pp
+else
+	echo "not implemented."
+fi
